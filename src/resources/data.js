8496af622ebfd80d134ac46bba686522ab34d0a5
Sri Balaji M <srisvit@gmail.com>
2023-01-12 12:38:57 -0500

    init commit

---
+++ src/resources/data.js
@@ -0,0 +1,307 @@
+const data = {
+    personal_details: {
+        picture: "profile-pic.jpg",
+        name: "Sri Balaji Muruganandam",
+        github: "https://github.com/iSriBalaji",
+        github_short: "iSriBalaji",
+        linkedin: "https://www.linkedin.com/in/isribalaji",
+        linkedin_short: "iSriBalaji",
+        email: "sribalaji0203@gmail.com",
+        location: "Buffalo, New York, US",
+        highest_codechef_rating: "0",
+        description: [
+            "‣ An enthusiastic engineer who loves to solve problems with data and software",
+            "‣ I am a bright student learning continuously stuffs on machine learning, data engineering and cloud",
+            "‣ I keep myself occupied with fascinating things such as startups, tech, certifications, research and more",
+            "‣ I love reading books, meeting new people, voleentering for startup events, practising mindfulness"
+        ],
+        highlights: [
+            "Machine Learning",
+            "Google Cloud",
+            "BigQuery",
+            "Vertex AI",
+            "Tensorflow",
+            "Neural Networks",
+            "ETL",
+            "Airflow",
+            "Apache Spark"
+        ]
+    },
+    experience: [
+        {
+            company: "Fellowship.ai",
+            location: "Hawaii, US(Remote)",
+            title: "Data Science Intern",
+            start_date: "June, 2022",
+            end_date: "August, 2022",
+            description: "Modeled BERT based transformer model to answer plant based nutition questions by collecting data from over 2500+ videos, blogs and podcasts",
+            logo: "logos/fellowship.png"
+        },
+        {
+            company: "Tata Consultancy Services (Digital)",
+            location: "Chennai, India",
+            title: "Data Engineer",
+            start_date: "August, 2020",
+            end_date: "July, 2021",
+            description: "Built ETL data pipelines on Google Cloud with data fusion using GCS as data lake and BigQuery as data warehouse. Orchestrated pipelines with Apache Airflow(Cloud Composer)",
+            logo: "logos/tcs.png"
+        },
+        {
+            company: "KPMG",
+            location: "Chennai, India(Remote)",
+            title: "Data Analytics Virtual Internship",
+            start_date: "June, 2020",
+            end_date: "July, 2020",
+            description: "Worked on three modules given a dataset - Data Cleansing, Exploring, Visualization and Presentation   ",
+            logo: "logos/kpmg.png"
+        }
+    ],
+    education: [
+        {
+            institution: "University at Buffalo, The State University of New York",
+            logo: "logos/ub.svg",
+            type: "College",
+            course: "Data Sciences and Applications",
+            short_course: "Masters of Professional Studies",
+            start_year: "2021",
+            end_year: "2023",
+            awards: [
+                "Finalist of Buffalo Startup Weekend 2022",
+                "Volenteering for BSW'22 and Startup Coffee"
+            ]
+        },
+        {
+            institution: "Anna University",
+            logo: "logos/annauniv.png",
+            type: "College",
+            course: "Electronics and Communication Engineering",
+            short_course: "Bachelors of Engineering",
+            start_year: "2016",
+            end_year: "2020",
+            awards: [
+                "58th All India Rank in National Creativity Aptitude Test",
+                "Winner of ”Design Your Destiny 2019” hardware hackathon among 5000 teams all over India",
+                "Best Paper Award - International Conference on Research Advancements & Challenges in Engineering Sciences (ICRACE’20)",
+                "Editor of Department Newsletter(2018 & 2019)",
+            ]
+        },
+        {
+            institution: "SRM University",
+            logo: "logos/srm.png",
+            type: "College",
+            course: "Yoga for Human Excellence",
+            short_course: "Diploma",
+            start_year: "2014",
+            end_year: "2016",
+            awards: [
+                "Leanred the Practical and Theory of Physical, Mental, Social and Spiritual Health.",
+                "Excercised and Meditated daily for 40 mins before starting the classes during high school"
+            ]
+        }
+    ],
+    courses: [
+        {
+            title: "Google Cloud Professional Data Engineer",
+            provider: "Google Cloud",
+            certificate_link: "https://www.credential.net/a9b6663f-8e2c-4d8f-93be-ae5609f82ba0"
+        },
+        {
+            title: "Programming, Data Structures and Algorithms using Python",
+            provider: "IIT Madras, NPTEL",
+            certificate_link: "https://drive.google.com/file/d/1GZ-MoORM5luoOnqAOGDjlrz9vHkwpL2S/view?usp=share_link",
+            description: ["Elite + Silver"]
+        },
+        {
+            title: "AWS Certified Cloud Practitioner",
+            provider: "Amazon Web Services",
+            certificate_link: "https://www.credly.com/badges/e8ba50ba-d6cd-4244-b484-ad5cf0b6b650/public_url"
+        },
+        {
+            title: "Tensorflow Developer Certification",
+            provider: "Google",
+            description: ["In Progress"]
+        },
+        {
+            title: "Image Processing using Cloud",
+            provider: "Techfest, IIT Bombay",
+            certificate_link: "https://www.prismcode.in/certificate/?i=Tm1FeE5UaGpNV0V5WkRNMVlqWTJZbVk1TURrME16RTVaVEJrT1RReVpURXdOamhqWWpobU5TMVFRMVJKTWpVPQ==&s=WW1GbFpHSmtNVGRrTTJFeU9HWTRaV1psT0dRME9ETmlaamd3WW1ReVl6WTNZelV6TnpBek5pMDU="
+        },
+        {
+            title: "Big Data with Apache Spark and Python",
+            provider: "Udemy",
+            certificate_link: "https://www.udemy.com/certificate/UC-d49103ce-a0bc-4a9e-a7d4-a7215fb9794d/"
+        },
+        {
+            title: "Machine Learning",
+            provider: "Andrew NG, Coursera"
+        },
+        {
+            title: "Sentiment Analysis in Python",
+            provider: "DataCamp",
+            certificate_link: "https://www.datacamp.com/statement-of-accomplishment/course/fec52a7cde63ec651f9dbccba4a4c256e5dd3915"
+        },
+        {
+            title: "Image Processing with Keras in Python",
+            provider: "DataCamp",
+            certificate_link: "https://www.datacamp.com/statement-of-accomplishment/course/1718dcc6e8a7b53361079851b4001b5c33503e16"
+        },
+        {
+            title: "Introduction to Deep Learning in Python",
+            provider: "DataCamp",
+            certificate_link: "https://www.datacamp.com/statement-of-accomplishment/course/d6125bd3a99ec6dcf482d15bcd8840bb15752998"
+        },
+        {
+            title: "Hadoop Linkedin Certification",
+            provider: "Linkedin Learning Courses®",
+            certificate_link: "https://drive.google.com/file/d/1HjcQU2fJWCQpoo4LDqRXfBteQa_jqAOK/view?usp=sharing"
+        }
+    ],
+    projects: [
+        {
+            title: "Sentiment Analysis on Social Engagement Platform",
+            description: ["Developed for Morgan Stanley - Code to give hackathon",
+                            "Performs Sentiment analysis on users post using Natural Language Processing",
+                        "Uses Google Analytics to measure user activity on the platform",
+                    "SEO optimized application built on Django framework"],
+            tools: [
+                "Python",
+                "Django",
+                "Heroku",
+                "Postgres",
+                "NLP",
+                "HTML/CSS/JS",
+                "AWS S3"
+            ],
+            github: "https://github.com/iSriBalaji/Code-To-Give-Mavericks",
+            check_out: "https://atlanta-mission.herokuapp.com/"
+        },
+        {
+            title: "Clinicon - Medical Assistance for Rural People",
+            description: ["Full stack desktop application to predict diseases with greatest likelihood without any human intervention",
+                        "Uses Google Speech to Text API convert patients speech to text and eSpeak synthesizer to convert text to speech",
+                        "Patent No: 202041009208 | Intellectual Property India"],
+            tools: [
+                "Python",
+                "Firebase",
+                "UI - tkinter",
+                "Classification",
+                "Raspberry Pi",
+            ],
+            github: "https://github.com/iSriBalaji/Clinicon-Medical-Assistant",
+            check_out: "https://www.youtube.com/watch?v=pA6wOhfTQgU&ab_channel=VelammalItechECE"
+        },
+        {
+            title: "911 – What & How Well Do They Serve",
+            description: ["Performed EDA to analyze and visualize uncovered structure of the New York state’s 911 dataset",
+                        "Modeled regression to predict response time of future crimes and classified incidents by logistic regression with accuracy of 83%",
+                        ],
+            tools: [
+                "Python",
+                "SQLite",
+                "Regression",
+                "Classification",
+                "Data Visualization",
+            ],
+            github: "https://github.com/iSriBalaji/911-Analysis",
+            check_out: "https://docs.google.com/document/d/1-IknBpRzrvBQYOJO4tJZJ7_5HUE7n4qZ0NDiaCdMbHA/edit"
+        },
+    ],
+    clubs: [
+        {
+            name: "How I Braved Anu Aunty and Co-Founded A Million Dollar Company",
+            role: "I read a few pages regularly in the school bus when I was in my 5th semester",
+            logo: "logos/hb.jpg"
+        },
+        {
+            name: "Trailblazer: The Power of Business as the Greatest Platform for Change",
+            role: "October 2022",
+            logo: "logos/trail.jpg"
+        },
+        {
+            name: "The Happiest Man on Earth: The Beautiful Life of an Auschwitz Survivor",
+            role: "October 2022",
+            logo: "logos/happy.jpg",
+            description: [
+                "When I completed this book, I understood how lucky we are"
+            ]
+        },
+        {
+            name: "Awareness",
+            role: "November 2022",
+            logo: "logos/awareness.jpg",
+            description: [
+                "This book made me realized that I am a Spritual person too. I started meditating regularly again; practising mindfulness"
+            ]
+        },
+        {
+            name: "The Subtle Art of Not Giving a F*ck",
+            role: "December 2022",
+            logo: "logos/subtle.jpg"
+        },
+        {
+            name: "Never Split the Difference: Negotiating As If Your Life Depended On It",
+            role: "January 2023",
+            logo: "logos/nego.jpeg"
+        },
+        {
+            name: "A Man Called Ove",
+            role: "December 2022",
+            logo: "logos/ove.jpg",
+        }
+    ],
+    skills: [
+        {
+            title: "Tools and Technologies",
+            list: ["Python", "Octave", "C", "Linux", "Git", "Tableau", "Jenkins", "Jira", "Confluence", "SharePoint", "Machine Learning"]
+        },
+        {
+            title: "Data Science & ML",
+            list: ["Tensorflow", "keras", "scikit-learn", "pandas", "numpy", "CNN", "RNN", "NLP", "data preprocessing", "visualization"]
+        },
+        {
+            title: "Data Engineering",
+            list: ["Apache Spark(Scripting, SQL)", "Airflow", "Shell Scripting", "AWS EMR", "Oracle SQL", "PL/SQL", "ETL"]
+        },
+        {
+            title: "Cloud Products",
+            list: ["Google Cloud Storage", "Data Fusion", "BigQuery", "Composer(Airflow)", "Dataproc", "S3", "EC2", "Lambda"]
+        }
+    ],
+    programming_achievements: [
+        {
+            title: "All India Rank 58 in National Creative Aptitude Test(NCAT)",
+            description: "The NCAT is designed to sensitize the students to the domain of creative thinking and motivate them to read, learn and practice the same.",
+            date: "2019"
+        },
+        {
+            title: "Winner - Code to Give Hackathon (Morgan Stanley)",
+            description: "Developed an SEO-optimized full-stack social engagement platform to connect stakeholders of an NGO (Atlanta Mission) with its clients. Appreciated with $500 prize",
+            date: "2022"
+        },
+        {
+            title: "Finalist - Smart India Hackathon 2020",
+            description: "Selected as top 5 among thousands of teams all around India",
+            date: "2020"
+        },
+        {
+            title: "Best Paper Award - International Conference on Research Advancements & Challenges in Engineering Sciences (ICRACE'20)",
+            description: "Received Best Paper Award for presenting a prototype for Social Security along with simulations.",
+            date: "2020"
+        },
+        {
+            title: "Winner of Design Your Destiny'2019 (Electrical Stream)Won Codechef DSA Certification Scholarship for excellent performance in their monthly challenge",
+            description: "Won the hardware hackathon among 5000 teams all over India by clearing multiple rounds of preseantaions and demos over the period of 3 months. Appreciated with $500 prize",
+            date: "2019"
+        },
+        {
+            title: "Finalist of Buffalo Startup Weekend 2022 and volunteering for Startup Week & Startup Coffee",
+            date: "2022"
+        },
+        {
+            title: "Won in 5 Project fests and 7 Paper presentations across India",
+            date: "2017-2020"
+        }
+    ]
+}
+
+export default data;
