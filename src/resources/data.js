44206bbb455243a05af5b5c1e01c0a70d8edbae7
Sri Balaji M <srisvit@gmail.com>
2023-01-12 02:28:40 -0500

    Init Commit

---
+++ src/resources/data.js
@@ -0,0 +1,285 @@
+const data = {
+    personal_details: {
+        picture: "profile-pic.jpg",
+        name: "Sri Balaji Muruganandam",
+        github: "https://github.com/iSriBalaji",
+        github_short: "iSriBalaji",
+        linkedin: "https://www.linkedin.com/in/isribalaji",
+        linkedin_short: "iSriBalaji",
+        email: "sribalaji0203@gmail.com",
+        location: "Buffalo, New York, US",
+        highest_codechef_rating: "0",
+        description: [
+            "‣ An enthusiastic engineer who loves to solve problems with data and software",
+            "‣ I am a bright student learning continuously stuffs on machine learning, data engineering and cloud",
+            "‣ I keep myself occupied with fascinating things such as startups, tech, certifications, research and more",
+            "‣ I love reading books, meeting new people, voleentering for startup events, practising mindfulness"
+        ],
+        highlights: [
+            "Machine Learning",
+            "Google Cloud",
+            "BigQuery",
+            "Vertex AI",
+            "Tensorflow",
+            "Neural Networks",
+            "ETL",
+            "Airflow",
+            "Apache Spark"
+        ]
+    },
+    experience: [
+        {
+            company: "Fellowship.ai",
+            location: "Hawaii, US(Remote)",
+            title: "Data Science Intern",
+            start_date: "June, 2022",
+            end_date: "August, 2022",
+            description: "Modeled BERT based transformer model to answer plant based nutition questions by collecting data from over 2500+ videos, blogs and podcasts",
+            logo: "logos/fellowship.png"
+        },
+        {
+            company: "Tata Consultancy Services (Digital)",
+            location: "Chennai, India",
+            title: "Data Engineer",
+            start_date: "August, 2020",
+            end_date: "July, 2021",
+            description: "Built ETL data pipelines on Google Cloud with data fusion using GCS as data lake and BigQuery as data warehouse. Orchestrated pipelines with Apache Airflow(Cloud Composer)",
+            logo: "logos/tcs.png"
+        },
+        {
+            company: "KPMG",
+            location: "Chennai, India(Remote)",
+            title: "Data Analytics Virtual Internship",
+            start_date: "June, 2020",
+            end_date: "July, 2020",
+            description: "Worked on three modules given a dataset - Data Cleansing, Exploring, Visualization and Presentation   ",
+            logo: "logos/kpmg.png"
+        }
+    ],
+    education: [
+        {
+            institution: "University at Buffalo, The State University of New York",
+            logo: "logos/ub.svg",
+            type: "College",
+            course: "Data Sciences and Applications",
+            short_course: "Masters of Professional Studies",
+            start_year: "2021",
+            end_year: "2023",
+            awards: [
+                "Finalist of Buffalo Startup Weekend 2022",
+                "Volenteering for BSW'22 and Startup Coffee"
+            ]
+        },
+        {
+            institution: "Anna University",
+            logo: "logos/annauniv.png",
+            type: "College",
+            course: "Electronics and Communication Engineering",
+            short_course: "Bachelors of Engineering",
+            start_year: "2016",
+            end_year: "2020",
+            awards: [
+                "58th All India Rank in National Creativity Aptitude Test",
+                "Winner of ”Design Your Destiny 2019” hardware hackathon among 5000 teams all over India",
+                "Best Paper Award - International Conference on Research Advancements & Challenges in Engineering Sciences (ICRACE’20)",
+                "Editor of Department Newsletter(2018 & 2019)",
+            ]
+        },
+        {
+            institution: "SRM University",
+            logo: "logos/srm.png",
+            type: "College",
+            course: "Yoga for Human Excellence",
+            short_course: "Diploma",
+            start_year: "2014",
+            end_year: "2016",
+            awards: [
+                "Leanred the Practical and Theory of Physical, Mental, Social and Spiritual Health.",
+                "Excercised and Meditated daily for 40 mins before starting the classes during high school"
+            ]
+        }
+    ],
+    courses: [
+        {
+            title: "Complete Web development bootcamp",
+            provider: "Angela Yu, Udemy",
+            certificate_link: "https://www.udemy.com/certificate/UC-7d02c806-6a5b-44c6-b1a1-5d4dd5a45e4a/"
+        },
+        {
+            title: "Python for Data Science",
+            provider: "IIT Madras, NPTEL",
+            certificate: "certificates/pds_nptel.png",
+            description: ["Elite + Silver", "Ranked in Top 1% of certified candidates"]
+        },
+        {
+            title: "Programming in Java",
+            provider: "IIT Kharagpur, NPTEL",
+            certificate: "certificates/java_nptel.png",
+            description: ["Elite + Gold", "Topped the course with a score of 99%"]
+        },
+        {
+            title: "Programming, Data Structures and Algorithms in Python",
+            provider: "IIT Madras, NPTEL",
+            certificate: "certificates/python_nptel.png",
+            description: ["Elite + Gold", "Topped the course with a perfect score of 100%"]
+        },
+        {
+            title: "App development using Android Studio with Java",
+            provider: "Tim Buchalka, Udemy",
+            certificate_link: "https://www.udemy.com/certificate/UC-f9557049-04be-4d85-a2a7-f2232c336dea/"
+        },
+        {
+            title: "Data Structures and Algorithms using C/C++",
+            provider: "Abdul Bari, Udemy",
+            certificate_link: "https://www.udemy.com/certificate/UC-7ca45d0f-99d6-4b1d-a501-b4c15e80435c/"
+        },
+        {
+            title: "Machine Learning",
+            provider: "Andrew NG, Coursera"
+        },
+        {
+            title: "Zoho Certified Student Developer",
+            provider: "ITK Education Services Private Limited & NSTI - Government of India"
+        },
+        {
+            title: "Data Structures and Algorithms",
+            provider: "College Curriculum"
+        },
+        {
+            title: "Database Management System",
+            provider: "College Curriculum"
+        },
+        {
+            title: "Operating Systems",
+            provider: "College Curriculum"
+        },
+        {
+            title: "Computer Networks and Applications",
+            provider: "College Curriculum"
+        },
+        {
+            title: "Machine Learning and Artificial Intelligence",
+            provider: "College Curriculum"
+        }
+    ],
+    projects: [
+        {
+            title: "Personal Portfolio Website",
+            description: [],
+            tools: [
+                "React",
+                "React Router"
+            ],
+            github: "https://github.com/harrisonvijay/harrisonvijay.github.io"
+        },
+        {
+            title: "YouShare",
+            description: ["A place where you can share your images and thoughts"],
+            tools: [
+                "NodeJS",
+                "Express",
+                "MongoDB",
+                "Mongo Atlas",
+                "React",
+                "React Redux",
+                "Redux Thunk",
+                "React Router",
+                "Google OAuth",
+                "bcryptjs"
+            ],
+            github: "https://github.com/harrisonvijay/YouShare",
+            check_out: "https://youshare-app.herokuapp.com/"
+        },
+    ],
+    clubs: [
+        {
+            name: "Institute of Electrical and Electronics Engineers (IEEE)",
+            role: "Student Member",
+            logo: "logos/ieee.png"
+        },
+        {
+            name: "The Association for Computing Machinery (ACM)",
+            role: "Student Member",
+            logo: "logos/acm.png"
+        },
+        {
+            name: "Youth Red Cross (YRC)",
+            role: "Member and Volunteer",
+            logo: "logos/yrc.jpg",
+            description: [
+                "Visited several villages, cleaned roads, rallied for awareness about the effects of plastic on the environment, and conducted a census",
+                "Visited Indian Red Cross hospital to learn about Physiotherapy, Blood group detection and Vaccines for Babies",
+                "Volunteered for multiple events of YRC in College"
+            ]
+        },
+        {
+            name: "IEEE Computer Society",
+            role: "Secretary, College Chapter",
+            logo: "logos/ieee_compsoc.jpg"
+        },
+        {
+            name: "ProCoDe, Intradepartment coding club",
+            role: "Member and Mentor",
+            logo: "logos/procode.jpg",
+            description: [
+                "Organized a 3-day workshop on the basics of Competitive Programming",
+                "Interaction with Juniors regarding placements and competitive programming"
+            ]
+        },
+        {
+            name: "Lakshya, Entrepreneurship Development Cell, SSN",
+            role: "Member",
+            logo: "logos/lakshya.jpg",
+        }
+    ],
+    skills: [
+        {
+            title: "Tools and Technologies",
+            list: ["Python", "Octave", "C", "Linux", "Git", "Tableau", "Jenkins", "Jira", "Confluence", "SharePoint", "Machine Learning"]
+        },
+        {
+            title: "Data Science & ML",
+            list: ["Tensorflow", "keras", "scikit-learn", "pandas", "numpy", "CNN", "RNN", "NLP", "data preprocessing", "visualization"]
+        },
+        {
+            title: "Data Engineering",
+            list: ["Apache Spark(Scripting, SQL)", "Airflow", "Shell Scripting", "AWS EMR", "Oracle SQL", "PL/SQL", "ETL"]
+        },
+        {
+            title: "Cloud Products",
+            list: ["Google Cloud Storage", "Data Fusion", "BigQuery", "Composer(Airflow)", "Dataproc", "S3", "EC2", "Lambda"]
+        }
+    ],
+    programming_achievements: [
+        {
+            title: "Highest Rating of 1824 in Codechef"
+        },
+        {
+            title: "Ranked 1283rd globally in Google Hashcode",
+            date: "2021"
+        },
+        {
+            title: "Ranked 574th globally in Google Kickstart Round H",
+            date: "2020"
+        },
+        {
+            title: "Global Rank 99 and India Rank 8 in IEEEXtreme 14.0 (out of 3700+ teams)",
+            description: "IEEEXtreme is a 24-hour programming contest conducted by IEEE",
+            date: "2020"
+        },
+        {
+            title: "Won Codechef DSA Certification Scholarship for excellent performance in their monthly challenge",
+            date: "January, 2020"
+        },
+        {
+            title: "Ranked 1st among 100+ participants in the annual intra-department coding contest",
+            date: "2019"
+        },
+        {
+            title: "Global Rank 176 and India Rank 15 in IEEEXtreme 13.0 (out of 4000+ teams)",
+            date: "2019"
+        }
+    ]
+}
+
+export default data;
