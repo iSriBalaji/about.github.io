8fe214de8c72755430fc66af1729461754647a66
Sri Balaji Muruganandam <66082342+iSriBalaji@users.noreply.github.com>
2023-01-12 14:30:29 -0500

    Updates

---
+++ precache-manifest.baad0a06ee1e90e3d746bc7e31b7b21c.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e9ff5fd7d3fa7e192ff",
+    "url": "/sri/static/css/main.e8f83d42.chunk.css"
+  },
+  {
+    "revision": "8e9ff5fd7d3fa7e192ff",
+    "url": "/sri/static/js/main.8e9ff5fd.chunk.js"
+  },
+  {
+    "revision": "7125b81a5d8d42bd4c8d",
+    "url": "/sri/static/js/1.7125b81a.chunk.js"
+  },
+  {
+    "revision": "b20ff617f231391ddbc0",
+    "url": "/sri/static/js/runtime~main.b20ff617.js"
+  },
+  {
+    "revision": "2884180c11fbd3119c7b55b3fd44083c",
+    "url": "/sri/static/media/java_nptel.2884180c.png"
+  },
+  {
+    "revision": "0b972fa380c829acceb7bb0b61218a98",
+    "url": "/sri/static/media/pds_nptel.0b972fa3.png"
+  },
+  {
+    "revision": "7a312c6b39e58a17cf0fcb5b9e066d13",
+    "url": "/sri/static/media/python_nptel.7a312c6b.png"
+  },
+  {
+    "revision": "59793f513e30a8b065b82ffc7bb00f67",
+    "url": "/sri/static/media/acm.59793f51.png"
+  },
+  {
+    "revision": "d589cd7263ba830b173a92f1742ef124",
+    "url": "/sri/static/media/annauniv.d589cd72.png"
+  },
+  {
+    "revision": "c5dff3f280afa4fca5d144c6a99317ef",
+    "url": "/sri/static/media/awareness.c5dff3f2.jpg"
+  },
+  {
+    "revision": "2c39d91176147d193488b259b5758764",
+    "url": "/sri/static/media/citi.2c39d911.jpg"
+  },
+  {
+    "revision": "d7f27f482051c415e871ddd7f78cc6e3",
+    "url": "/sri/static/media/fellowship.d7f27f48.png"
+  },
+  {
+    "revision": "8dc0343d76e1e7ac6be65a4deac94acc",
+    "url": "/sri/static/media/happy.8dc0343d.jpg"
+  },
+  {
+    "revision": "90bdd1e2aae65aa6b5afb6e696234246",
+    "url": "/sri/static/media/hb.90bdd1e2.jpg"
+  },
+  {
+    "revision": "b9e5d656967aa4dd59e7f44230c5eab2",
+    "url": "/sri/static/media/itkes.b9e5d656.png"
+  },
+  {
+    "revision": "cd6b7be55dfb948af92c50f134514009",
+    "url": "/sri/static/media/kpmg.cd6b7be5.png"
+  },
+  {
+    "revision": "9f7eda78d08f503d7092ff710bbb64f1",
+    "url": "/sri/static/media/lakshya.9f7eda78.jpg"
+  },
+  {
+    "revision": "de5863c9dbec438b9684f2da048c533a",
+    "url": "/sri/static/media/marthoma.de5863c9.png"
+  },
+  {
+    "revision": "b68bb1ba4dbe33725342406765aa3a32",
+    "url": "/sri/static/media/nego.b68bb1ba.jpeg"
+  },
+  {
+    "revision": "488228cb467c49937078ff5956545257",
+    "url": "/sri/static/media/ove.488228cb.jpg"
+  },
+  {
+    "revision": "cc5caff244ba80ec8e0ffc4928a37104",
+    "url": "/sri/static/media/procode.cc5caff2.jpg"
+  },
+  {
+    "revision": "e7bfa93e64c755d7c5322fdc7f959212",
+    "url": "/sri/static/media/srm.e7bfa93e.png"
+  },
+  {
+    "revision": "fe6b9ca84d67bc080652f0d4b3f0466a",
+    "url": "/sri/static/media/ssn.fe6b9ca8.svg"
+  },
+  {
+    "revision": "0b22195adff029171edf6ab5dc251b7b",
+    "url": "/sri/static/media/subtle.0b22195a.jpg"
+  },
+  {
+    "revision": "1b6aaefb55f90b102bad84d4fff02c85",
+    "url": "/sri/static/media/tcs.1b6aaefb.png"
+  },
+  {
+    "revision": "238bc03bc63161df1a3155eb8bc4f5d7",
+    "url": "/sri/static/media/trail.238bc03b.jpg"
+  },
+  {
+    "revision": "14c537522d694529a66b2c9d8c51eb16",
+    "url": "/sri/static/media/ub.14c53752.svg"
+  },
+  {
+    "revision": "45a0c9ce83a7b254b4b047d654ad03a3",
+    "url": "/sri/static/media/profile-pic.45a0c9ce.jpg"
+  },
+  {
+    "revision": "ed841c2c09cc2426faa7aa133accdea4",
+    "url": "/sri/index.html"
+  }
+];
