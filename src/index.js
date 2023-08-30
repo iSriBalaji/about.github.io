df848b50189f2912b51df765130a4ed65d87df3d
Sri Balaji M <srisvit@gmail.com>
2023-08-30 15:56:18 -0400

    update ui

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// const globals = require.resolve('./globals.json');
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') 
 );
